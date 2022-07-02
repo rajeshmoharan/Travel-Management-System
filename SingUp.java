c1f17b44c483f701496a705f427874a456b0934f
rajeshmoharan <98909621+rajeshmoharan@users.noreply.github.com>
2022-07-02 23:01:32 +0530

    added sql query
    databse is connected

--- SingUp.java
+++ SingUp.java
@@ -3,10 +3,19 @@ import java.awt.Color;
 
 import java.awt.Font;
 import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.*;
 
-public class SingUp extends JFrame {
+public class SingUp extends JFrame implements ActionListener {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	JButton create,back;
+	JTextField tfusername,tfpassword,tfname,tfmobile,tfgmail;
 	
 	SingUp(){
 		setSize(1000,400);
@@ -21,12 +30,23 @@ public class SingUp extends JFrame {
 		p1.setLayout(null);
 		add(p1);
 		
-		JLabel email=new JLabel("Gmail");
-		email.setBounds(20,60,80,30);
-		email.setFont(new Font("Aohma",Font.BOLD,14));
-		p1.add(email);
+		JLabel ibgmail=new JLabel("Gmail");
+		ibgmail.setBounds(20,20,50,30);
+		ibgmail.setFont(new Font("Aohma",Font.BOLD,14));
+		p1.add(ibgmail);
 		
-		JTextField tfusername=new JTextField();
+		tfgmail=new JTextField();
+		tfgmail.setBounds(110,20,200,30);
+		tfgmail.setBorder(null);
+		p1.add(tfgmail);
+		
+		
+		JLabel ibusername=new JLabel("username");
+		ibusername.setBounds(20,60,80,30);
+		ibusername.setFont(new Font("Aohma",Font.BOLD,14));
+		p1.add(ibusername);
+		
+		tfusername=new JTextField();
 		tfusername.setBounds(110,60,200,30);
 		tfusername.setBorder(null);
 		p1.add(tfusername);
@@ -36,7 +56,7 @@ public class SingUp extends JFrame {
 		jlname.setFont(new Font("Aohma",Font.BOLD,14));
 		p1.add(jlname);
 		
-		JTextField tfname=new JTextField();
+		tfname=new JTextField();
 		tfname.setBounds(110,110,200,30);
 		tfname.setBorder(null);
 		p1.add(tfname);
@@ -46,7 +66,7 @@ public class SingUp extends JFrame {
 		ibpassword.setFont(new Font("Ahoma",Font.BOLD,14));
 		p1.add(ibpassword);
 		
-		JTextField tfpassword=new JTextField();
+		tfpassword=new JTextField();
 		tfpassword.setBounds(110, 210, 200, 30);
 		tfpassword.setBorder(null);
 		p1.add(tfpassword);
@@ -56,23 +76,25 @@ public class SingUp extends JFrame {
 		mobile.setFont(new Font("Ahoma",Font.BOLD,14));
 		p1.add(mobile);
 		
-		JTextField tfemail=new JTextField();
-		tfemail.setBounds(110, 160, 200, 30);
-		tfemail.setBorder(null);
-		p1.add(tfemail);
+		tfmobile=new JTextField();
+		tfmobile.setBounds(110, 160, 200, 30);
+		tfmobile.setBorder(null);
+		p1.add(tfmobile);
 		
-		JButton create=new JButton("Create");
+		create=new JButton("Create");
 		create.setBounds(60, 280, 100, 40);
 		create.setBackground(Color.GRAY);
 		create.setForeground(Color.white);
 		create.setBorder(null);
+		create.addActionListener(this);
 		p1.add(create);		
 		
-		JButton back=new JButton("Back");
+		back=new JButton("Back");
 		back.setBounds(220, 280, 100, 40);
 		back.setBackground(Color.GRAY);
 		back.setForeground(Color.white);
 		back.setBorder(null);
+		back.addActionListener(this);
 		p1.add(back);		
 		
 		
@@ -89,6 +111,32 @@ public class SingUp extends JFrame {
 		
 		
 		setVisible(true);
+		
+	}
+	public void actionPerformed(ActionEvent ae) {
+		if(ae.getSource() == create) {
+			String username=tfusername.getText();
+			String name=tfname.getText();
+			String mobile=tfmobile.getText();
+			String gmail=tfgmail.getText();
+			
+			String query ="insert into account values('"+username+"','"+name+"','"+mobile+"','"+gmail+"')";
+			try {
+				Conn c=new Conn();
+				c.s.executeUpdate(query);
+				
+				
+				JOptionPane.showMessageDialog(null, "Account Createed Succesfully");
+				setVisible(false);
+				new Login();
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+					
+		}else if (ae.getSource() == back){
+			setVisible(false);
+			new Login();
+		}
 		
 	}
 
